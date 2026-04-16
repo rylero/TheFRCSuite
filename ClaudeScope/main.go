1f6d385a1ec1415eb4a56fc625013e9a4e9805ad
Ryan LeRoy <leroy.ryan09@gmail.com>
2026-04-16 10:54:08 -0700

    refactor: remove REPL prototype, scaffold new package dirs

--- ClaudeScope/main.go
+++ ClaudeScope/main.go
@@ -1,33 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/levifitzpatrick1/go-nt4"
-)
-
-// realNTClient wraps go-nt4's client to satisfy the NTClient interface.
-type realNTClient struct{ c *nt4.Client }
-
-func (r *realNTClient) Connect() error { return r.c.Connect() }
-func (r *realNTClient) Disconnect()    { r.c.Disconnect() }
-
-func realFactory(ip string) (NTClient, error) {
-	opts := nt4.DefaultClientOptions(ip)
-	opts.Identity = "ClaudeScope"
-	c := nt4.NewClient(opts)
-	if err := c.Connect(); err != nil {
-		return nil, err
-	}
-	return &realNTClient{c}, nil
-}
-
-func main() {
-	fmt.Println("ClaudeScope v1.0.0")
-	fmt.Println("Type 'help' for available commands.")
-	fmt.Println()
-
-	session := NewRobotSession(realFactory)
-	RunREPL(os.Stdin, os.Stdout, session)
-}
+func main() {}
